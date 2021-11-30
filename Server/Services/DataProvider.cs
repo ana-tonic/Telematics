abf00e6cf3b212b463d5f2f4a115528822bcec70
Vukasin373 <vukasincvetkovic645@gmail.com>
2021-11-30 13:55:26 +0100

    asda

---
+++ Server/Services/DataProvider.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Server.
+
+namespace Server.Services
+{
+    public class DataProvider
+    {
+        private IDataStaxService Services { get; set; }
+        
+        
+
+        #region Deliveries
+        public static getAllDeliveries()
+        {
+            Services.
+        }
+
+
+        #endregion
+    }
+}
