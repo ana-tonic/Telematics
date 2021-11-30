83814395053cdb3a7c7e13ff95c80adb560a917f
Vukasin373 <vukasincvetkovic645@gmail.com>
2021-11-30 15:24:14 +0100

    asd

--- Server/DataProvider.cs
+++ Server/DataProvider.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Server.
+using Server;
 
 namespace Server.Services
 {
@@ -15,7 +15,7 @@ namespace Server.Services
         #region Deliveries
         public static getAllDeliveries()
         {
-            Services.
+          //  Deliveries 
         }
 
 
