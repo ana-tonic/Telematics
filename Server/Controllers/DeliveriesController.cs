beaafbfdbdc1bfa06a543db5238d86e0edaf933d
Aleksandar-Elfak <77788823+Aleksandar-Elfak@users.noreply.github.com>
2021-11-30 15:43:24 +0100

    a

--- Server/Controllers/DeliveriesController.cs
+++ Server/Controllers/DeliveriesController.cs
@@ -2,19 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Server.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class DeliveriesController
+    public class DeliveriesController : ControllerBase
     {
-      
+
 
         [HttpGet]
         public async Task<ActionResult<ICollection<Deliveries>>> GetAllDeliveries()
-        {   
-            
+        {
+
         }
     }
 }
--- Server/Services/DataProvider.cs
+++ Server/Services/DataProvider.cs
@@ -3,19 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Server;
+using Server.Interfaces;
 
 namespace Server.Services
 {
     public class DataProvider
     {
-        private IDataStaxService Services { get; set; }
-        
-        
+        private IDataStaxService Service { get; set; }
+
+
 
         #region Deliveries
         public static getAllDeliveries()
         {
-          //  Deliveries 
+            Services
         }
 
 
