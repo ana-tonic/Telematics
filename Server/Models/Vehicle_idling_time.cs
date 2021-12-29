b2d37d596b071415c6c8b436fb7a49c2d03c6068
Aleksandar-Elfak <77788823+Aleksandar-Elfak@users.noreply.github.com>
2021-12-29 19:07:05 +0100

    x

--- Server/Models/Vehicle_idling_time.cs
+++ Server/Models/Vehicle_idling_time.cs
@@ -7,7 +7,6 @@ namespace Server.Models
     public class Vehicle_idling_time
     {
         public int Truck_Id  { get; set; }
-        
         public int Delivery_Id { get; set; }
         public Duration Time_Idle { get; set; }
         public DateTimeOffset Reading_Time { get; set; }
--- Server/Services/DataProvider.cs
+++ Server/Services/DataProvider.cs
@@ -32,7 +32,7 @@ namespace Server.Services
                 Arrival_Time = d.Arrival_Time,
                 Cargo = d.Cargo,
                 Delivery_Id = Cassandra.TimeUuid.NewId(),
-                Departing_Time = DateTimeOffset.FromUnixTimeSeconds(new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds()),
+                Departing_Time = d.Departing_Time,
                 Driver = d.Driver,
                 End_Address = d.End_Address,
                 Start_Address = d.Start_Address,
@@ -43,7 +43,7 @@ namespace Server.Services
             RowSet del = Session.Execute("insert into telematics.Deliveries (cargo, year, active, departing_time, delivery_id, arrival_time, driver, end_address, start_address, truck_id)" // values ('" + delivery.Cargo + "', 1312, True, '2021-12-29T13:56:00.176Z', " + a + ", '2022-12-29T13:56:00.176Z', 'asda', '123', '123'," + b + ")");
                 //+ $" values ('{delivery.Cargo}', {delivery.Year}, {delivery.Active}, {delivery.Departing_Time}, {delivery.Delivery_Id}, {delivery.Arrival_Time}, '{delivery.Driver}', " + 
                 //$"'{delivery.End_Address}', '{delivery.Start_Address}', {delivery.Truck_Id})");
-                + $" values ('{delivery.Cargo}', {delivery.Year}, {delivery.Active}, '{delivery.Departing_Time}', {delivery.Delivery_Id}, '2021-12-29T13:56:00.176Z', '{delivery.Driver}', " +
+                + $" values ('{delivery.Cargo}', {delivery.Year}, {delivery.Active}, '{delivery.Departing_Time.ToUnixTimeMilliseconds()}', {delivery.Delivery_Id}, '2021-12-29T13:56:00.176Z', '{delivery.Driver}', " +
                 $"'{delivery.End_Address}', '{delivery.Start_Address}', {delivery.Truck_Id})");
 
         }
