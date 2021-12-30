45c057cff2bdceb5376a5aaf6e7a04a40def646c
Aleksandar-Elfak <77788823+Aleksandar-Elfak@users.noreply.github.com>
2021-12-30 13:05:24 +0100

    frontend deliveries start

--- Client/start.js
+++ Client/start.js
@@ -45,14 +45,62 @@ export class Start {
         this.drawDeliveryForm(buttonSection);
 
         const startDelivery = document.createElement("button");
-        startDelivery.className = "startDelivery, sectionbutton";
+        startDelivery.className = "startDelivery sectionbutton";
         buttonSection.appendChild(startDelivery);
+        startDelivery.onclick = () => {
+            console.log((this.container.querySelectorAll(".DFinput")));
+
+
+        }
         startDelivery.innerHTML = "Start delivery";
+
+        this.drawShowDeliveries(buttonSection);
 
         const showDelivery = document.createElement("button");
         showDelivery.className = "showDelivery, sectionbutton";
         buttonSection.appendChild(showDelivery);
+        showDelivery.onclick = () => {
+            const cargo = this.container.querySelector(".cargoSelect").value;
+            const year = this.container.querySelector(".year").value;
+            console.log(year);
+        }
         showDelivery.innerHTML = "Show deliveries";
+    }
+
+    drawDeliveryForm(host) {
+        const DF = document.createElement("div");
+        DF.className = "DF";
+        host.appendChild(DF);
+
+        this.drawDFormElement(DF, "Truck number: ", "number", "TruckID");
+        this.drawDFormElement(DF, "Driver: ", "text", "Driver");
+        this.drawDFormElement(DF, "Start Address: ", "text", "StartAddres");
+        this.drawDFormElement(DF, "End Address: ", "text", "EndAddress");
+
+        const cri = document.createElement("div");
+        cri.className = "elContainer";
+        DF.appendChild(cri);
+
+        const cargo = ["Iron ore",
+            "Coal",
+            "Cereal",
+            "Salt",
+            "Aluminum",
+            "Copper ore"];
+        let cargoSelect = document.createElement("select");
+        cargoSelect.className = "select cargoStart";
+        const lbl = document.createElement("label");
+        lbl.innerHTML = "Cargo: ";
+        cri.appendChild(lbl);
+
+        for (let i = 0; i < cargo.length; i++) {
+            const c = document.createElement("option");
+            c.value = cargo[i];
+            c.innerHTML = cargo[i];
+            cargoSelect.appendChild(c);
+        }
+
+        cri.appendChild(cargoSelect);
     }
 
     drawDFormElement(host, lblText, tip, className) {
@@ -66,20 +114,54 @@ export class Start {
 
         const el = document.createElement("input");
         el.type = tip;
-        el.className = className + ", DFinput";
+        el.className = className + " DFinput";
         elContainer.appendChild(el);
     }
 
+    drawShowDeliveries(host) {
+        const cri = document.createElement("div");
+        cri.className = "cri";
+        host.appendChild(cri);
 
-    drawDeliveryForm(host) {
-        const DF = document.createElement("div");
-        DF.className = "DF";
-        host.appendChild(DF);
+        const cargo = ["Iron ore",
+            "Coal",
+            "Cereal",
+            "Salt",
+            "Aluminum",
+            "Copper ore"];
+        let cargoSelect = document.createElement("select");
+        cargoSelect.className = "select cargoSelect";
+        const lbl = document.createElement("label");
+        lbl.innerHTML = "Cargo: ";
+        cri.appendChild(lbl);
 
-        this.drawDFormElement(DF, "Truck number: ", "number", "TruckID");
-        this.drawDFormElement(DF, "Driver: ", "text", "Driver");
-        this.drawDFormElement(DF, "Start Address: ", "text", "StartAddres");
-        this.drawDFormElement(DF, "End Address: ", "text", "EndAddress");
-        this.drawDFormElement(DF, "Cargo: ", "text", "Cargo");
+        for (let i = 0; i < cargo.length; i++) {
+            const c = document.createElement("option");
+            c.value = cargo[i];
+            c.innerHTML = cargo[i];
+            cargoSelect.appendChild(c);
+        }
+
+        cri.appendChild(cargoSelect);
+
+
+        const y = document.createElement("div");
+        y.className = "cri";
+        host.appendChild(y);
+
+        const lbly = document.createElement("label");
+        lbly.innerHTML = "Year";
+        y.appendChild(lbly);
+
+
+        const year = document.createElement("input");
+        year.type = "number";
+        year.value = new Date().getFullYear();
+        year.min = 2000;
+        year.max = 2040;
+        year.className = "year select";
+        y.appendChild(year);
     }
+
+
 }
--- Server/Models/Vehicle_speed.cs
+++ Server/Models/Vehicle_speed.cs
@@ -1,11 +1,12 @@
 using System;
+using Cassandra;
 
 namespace Server.Models
 {
     public class Vehicle_speed
     {
         public int Truck_Id { get; set; }
-        public int Delivery_Id { get; set; }
+        public Cassandra.TimeUuid Delivery_Id { get; set; }
         public int Speed { get; set; }
         public DateTimeOffset Reading_time { get; set; }
         public string Unit { get; set; }
