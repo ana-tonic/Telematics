a4fe1abd9e6a246e2041f93be5d8205698d97b47
Aleksandar-Elfak <77788823+Aleksandar-Elfak@users.noreply.github.com>
2021-12-30 19:04:35 +0100

    Update delivery.js

--- Client/delivery.js
+++ Client/delivery.js
@@ -1,14 +1,14 @@
 export class Delivery {
 
     constructor(cargo, year, active, departing_time, delivery_id, arrival_time, driver, end_address, start_address, truck_id) {
-        this.truckID = truck_id;
-        this.deliveryID = delivery_id;
+        this.truck_id = truck_id;
+        this.delivery_id = delivery_id;
         this.driver = driver;
-        this.startAddress = start_address;
-        this.endAddress = end_address;
+        this.start_address = start_address;
+        this.end_address = end_address;
         this.year = year;
-        this.departingTime = departing_time;
-        this.arrivalTime = arrival_time;
+        this.departing_time = departing_time;
+        this.arrival_time = arrival_time;
         this.cargo = cargo;
         this.active = active;
     }
